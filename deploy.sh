a4b78b2a102b5d3071efaf2ef30ca0f484ae2723
girgisdev <girgis.developer@yahoo.com>
2022-04-19 19:11:20 +0200

    ci: :wrench: updated travis file and added deploy.sh file

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t gergis/multi-container-client:latest -t gergis/multi-container-client:$SHA -f ./client/Dockerfile ./client
+docker build -t gergis/multi-container-server:latest -t gergis/multi-container-server:$SHA -f ./server/Dockerfile ./server
+docker build -t gergis/multi-container-worker:latest -t gergis/multi-container-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push gergis/multi-container-client:latest
+docker push gergis/multi-container-server:latest
+docker push gergis/multi-container-worker:latest
+
+docker push gergis/multi-container-client:$SHA
+docker push gergis/multi-container-server:$SHA
+docker push gergis/multi-container-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
